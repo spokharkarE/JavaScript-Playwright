b742a0318fc7ea0dc217735d595e2b15768da05d
APME-OFFICE\spokharkar <spokharkar@euronetworldwide.com>
2025-06-03 16:24:24 +0530

    Your commit message

---
+++ tests/PracticeJs/hello.js
@@ -0,0 +1,41 @@
+// //printing using a consloe 
+// console.log("hello JS")
+
+// //creating a variable and printing it 
+// var x =10;
+// console.log(x)
+
+// //creating a variable and printing it before a creation --> it gives is a undefined
+// var x =10;
+// console.log(x)
+
+// //craeting a function and calling after that
+// function f1(){
+//     console.log("inside the function")
+// }
+// f1();
+
+// //craeting a function and calling before that ---> it is also works not behave as a variable
+// f1();
+// function f1(){
+//     console.log("inside the function")
+// }
+
+// // crating a arrow function and calling after---> do not use function keyword ---> it works fine 
+// f2 = () => {
+//     console.log("inside the function")
+//     return 1;
+// }
+// f2();
+
+//arrow function and calling before---> do not use function keyword ---> it gives not defined error on before call it works as a variable
+// f2();
+// f2 = () => {
+//     console.log("inside the function")
+//     return 1;
+// }
+
+// we can make a variable as a undefined but it is not good way so dont make it. it is like for reserving a word or a varable
+// var a = undefined; // value is a undefined of a variable a here
+
+//
--- tests/first.spec.js
+++ tests/first.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://www.flipkart.com/');
-  await page.pause();
+  await page.pause()
   await page.getByRole('textbox', { name: 'Search for Products, Brands' }).click();
   await page.getByRole('textbox', { name: 'Search for Products, Brands' }).fill('galaxy');
   await page.getByRole('link', { name: 'galaxy a35 5g in Mobiles' }).click();
